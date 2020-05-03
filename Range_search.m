3e08c57c36eb525d1e0bcc5b2e6f2c6e75da44ee
Ramune6110 <Arduino6110@gmail.com>
2020-05-03 14:43:50 +0900

    [Fix]Commit

--- Range_search.m
+++ Range_search.m
@@ -2,21 +2,25 @@ clear;
 close all;
 clc;
 
-dt = 1;
-time = 0.0;
+dt      = 1;
+time    = 0.0;
 endtime = 50.0;
-nsteps = ceil((endtime - time) / dt);
+nsteps  = ceil((endtime - time) / dt);
 
-result.x = [];
-result.y = [];
+result.x    = [];
+result.y    = [];
 result.time = [];
+result.xEst = [];
+result.PEst = [];
 
-x = 10;
+x    = 10;
+xEst = 10;
+PEst = 1;
 w = 0.5^2;
 v = 0.05^2;
-a = -25;
-b = 25;
-N = 400;
+a = -20;
+b = 20;
+N = 500;
 % データセット
 % 一般に、区間 (a,b) の N 個の乱数は、式 r = a + (b-a).*rand(N,1) を使って生成できます。
 X_t = a + (b - a).*rand(N, 1);
@@ -42,23 +46,30 @@ for i = 1:N
     result.K = vertcat(result.K, result.temp');
     result.temp = [];
 end
-% % 平均
-% m = result.K(1,:) / (result.K + lamda_f * eye(N, N)) * result.X_t_1;
-% % 分散
-% sigma = alpha_f^2 - result.K(1,:) / (result.K + lamda_f * eye(N, N)) * (result.K(1,:))';
+% 平均
+m = result.K(1,:) / (result.K + lamda_f * eye(N, N)) * result.X_t_1;
+% 分散
+sigma = alpha_f^2 - result.K(1,:) / (result.K + lamda_f * eye(N, N)) * (result.K(1,:))';
+Upper = m + 3 * sqrt(sigma);
+Lower = m - 3 * sqrt(sigma);
 
-
-result.Temp = [];
-result.m = [];
+result.Temp_x  = [];
+result.Temp_xEst  = [];
+result.m     = [];
+result.m     = [result.m; m];
 result.sigma = [];
+result.sigma = [result.sigma; sigma];
 result.Upper = [];
+result.Upper = [result.Upper; Upper];
 result.Lower = [];
+result.Lower = [result.Lower; Lower];
+result.time  = [result.time; time];
+result.x     = [result.x; x];
+result.xEst  = [result.xEst; xEst];
+result.PEst  = [result.PEst; PEst];
 tic;% start　for文回る時間の計測
 for i = 1:nsteps
     time = time + dt;
-    % 関数値の計算
-    x = 0.2 * x + ((25 * x) / (1 + x^2)) + 8 * cos(1.2 * x) +  sqrt(w) * randn(1, 1);
-    y = sin(x / 10) + sqrt(v) * randn(1, 1);
     % k*(x_t)の計算
     for j = 1:N
         k = alpha_f^2 * exp((-1 / 2) * beta_f * (X_t(j, 1) -  x)^2);
@@ -70,7 +81,26 @@ for i = 1:nsteps
     sigma = alpha_f^2 - (result.Temp)' / (result.K + lamda_f * eye(N, N)) * result.Temp;
     Upper = m + 3 * sqrt(sigma);
     Lower = m - 3 * sqrt(sigma);
-
+    if time >= 25 && time <= 30 
+        % 関数値の計算
+        x = 0.2 * x + ((25 * x) / (1 + x^2)) + 8 * cos(1.2 * x) +  10 * sqrt(w) * randn(1, 1);
+        y = sin(x / 10) + 10 * sqrt(v) * randn(1, 1);
+    else
+        % 関数値の計算
+        x = 0.2 * x + ((25 * x) / (1 + x^2)) + 8 * cos(1.2 * x) +  sqrt(w) * randn(1, 1);
+        y = sin(x / 10) + sqrt(v) * randn(1, 1);
+    end
+    
+    %%  リスク鋭敏型フィルタ %%
+    % k*(x_t)の計算
+    for j = 1:N
+        k = alpha_f^2 * exp((-1 / 2) * beta_f * (X_t(j, 1) -  xEst)^2);
+        result.Temp_ = [result.Temp; k];
+    end
+    % 平均
+    m =  (result.Temp)' / (result.K + lamda_f * eye(N, N)) * result.X_t_1;
+    xPred = 
+    
     result.x = [result.x; x];
     result.y = [result.y; y];
     result.time = [result.time; time];
@@ -85,15 +115,11 @@ Drow(result);
 
 function [] = Drow(result)
     figure(1);
-    for i=2:50
-    re(i-1)=result.x(i,1);
-    end
-    re(50)=1;
-    plot(result.time, re, 'k'); hold on;
+    plot(result.time, result.x, 'k'); hold on;
     plot(result.time, result.Upper, 'r');hold on;
     plot(result.time, result.m, 'g'); hold on;
     plot(result.time, result.Lower, 'b');hold on;
-    figure(2);
-    plot(result.time, result.y, 'k');
+%     figure(2);
+%     plot(result.time, result.y, 'k');
     
 end
