2b0d6c28884eb122aedb615888aaf020cbe4b621
Ramune6110 <Arduino6110@gmail.com>
2020-05-07 14:59:58 +0900

    [Fix]Commit

--- Gaussian_process.m
+++ Gaussian_process.m
@@ -35,42 +35,20 @@ result.m     = [];
 result.sigma = [];
 result.Upper = [];
 result.Lower = [];
-%% データセットと状態方程式を基にX_t+1とY_tのデータセットを生成
-% 一般に、区間 (a,b) の N 個の乱数は、式 r = a + (b-a).*rand(N,1) を使って生成できます。
-X_t   = a + (b - a).*rand(N, 1);
-X_t_1 = zeros(N, 1); 
-Y_t   = zeros(N, 1);
-for i = 1:N
-    X_t_1(i, 1) = 0.2 * X_t(i, 1) + ((25 * X_t(i, 1)) / (1 + X_t(i, 1)^2)) + 8 * cos(1.2 * X_t(i, 1)) +  sqrt(w) * randn(1, 1);
-    Y_t(i, 1)   = sin(X_t(i, 1) / 10) + sqrt(v) * randn(1, 1);
-end
-%% データセットを基に関数fの平均と分散を計算
-K = zeros(N, N);
-for i = 1:N
-    for j = 1:N
-        K(i, j) = alpha_f^2 * exp((-1 / 2) * beta_f * (X_t(j, 1) -  X_t(i))^2);
-    end
-end
-%% データセットを基に関数hの平均と分散を計算
-L = zeros(N, N);
-for i = 1:N
-    for j = 1:N
-        L(i, j) = alpha_h^2 * exp((-1 / 2) * beta_h * (X_t(j, 1) -  X_t(i))^2);
-    end
-end
+%% データセット生成
+[X_t, X_t_1, Y_t, K, L] = makeDataset(a, b, w, v, alpha_f, alpha_h, beta_f, beta_h, N);
 m = K(1,:) / (K + lamda_f * eye(N, N)) * X_t_1;
 % 分散
 sigma = alpha_f^2 - K(1,:) / (K + lamda_f * eye(N, N)) * (K(1,:))';
 Upper = m + 3 * sqrt(sigma);
 Lower = m - 3 * sqrt(sigma);
 %% データセットから推定した関数の平均と分散を初期値として配列に格納
-result.m      = [result.m; m];
-result.sigma  = [result.sigma; sigma];
-result.Upper  = [result.Upper; Upper];
-result.Lower  = [result.Lower; Lower];
+result.m     = [result.m; m];
+result.sigma = [result.sigma; sigma];
+result.Upper = [result.Upper; Upper];
+result.Lower = [result.Lower; Lower];
 %% main loop
 tic;% start　
-
 for i = 1:nsteps
     time = time + dt;
     %% ガウス過程回帰による関数fの推定
@@ -95,11 +73,36 @@ for i = 1:nsteps
     result.Lower = [result.Lower; Lower];
 end
 toc;
-
 Drow(result);
 
+function [X_t, X_t_1, Y_t, K, L] = makeDataset(a, b, w, v, alpha_f, alpha_h, beta_f, beta_h, N)
+    %% データセットと状態方程式を基にX_t+1とY_tのデータセットを生成
+    % 一般に、区間 (a,b) の N 個の乱数は、式 r = a + (b-a).*rand(N,1) を使って生成できます。
+    X_t   = a + (b - a).*rand(N, 1);
+    X_t_1 = zeros(N, 1); 
+    Y_t   = zeros(N, 1);
+    for i = 1:N
+        X_t_1(i, 1) = 0.2 * X_t(i, 1) + ((25 * X_t(i, 1)) / (1 + X_t(i, 1)^2)) + 8 * cos(1.2 * X_t(i, 1)) +  sqrt(w) * randn(1, 1);
+        Y_t(i, 1)   = sin(X_t(i, 1) / 10) + sqrt(v) * randn(1, 1);
+    end
+    %% データセットを基に関数fの平均と分散を計算
+    K = zeros(N, N);
+    for i = 1:N
+        for j = 1:N
+            K(i, j) = alpha_f^2 * exp((-1 / 2) * beta_f * (X_t(j, 1) -  X_t(i))^2);
+        end
+    end
+    %% データセットを基に関数hの平均と分散を計算
+    L = zeros(N, N);
+    for i = 1:N
+        for j = 1:N
+            L(i, j) = alpha_h^2 * exp((-1 / 2) * beta_h * (X_t(j, 1) -  X_t(i))^2);
+        end
+    end
+end
+
 function k_star = Calc_k_star(N, alpha_f, beta_f, X_t, x)
-k_star = zeros(N, 1);
+    k_star = zeros(N, 1);
     for j = 1:N
         k_star(j, 1) = alpha_f^2 * exp((-1 / 2) * beta_f * (X_t(j, 1) -  x)^2);
     end
